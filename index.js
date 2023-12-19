0e4d53d4c7b61dc871ced38fca203e22fdc5a16d
mabiTLM <90094200+mabiTLM@users.noreply.github.com>
2023-12-19 14:09:33 +0900

    문서 경로 수정 확인용

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.getElementById("modal-box").onclick = (e) => {
 };
 
 const projectArr = {
-  firstProject: { details: "무작위로 선출한 3명씩 팀을 이뤄서 진행<br>팀장: 임현규, 팀원: 임주한, 팀원: 송성민<br><br>조원들과 토론 결과 요즘 음식점들은 개인 사이트를 원하는 경우가 많기에 수요가 또한 많을 것으로 보고 유명 프랜차이즈 레스토랑인 VIPS 사이트의 클론 코딩을 진행" },
+  firstProject: { details: "무작위로 선출한 3명씩 팀을 이뤄서 진행<br>팀장: 임현규, 팀원: 임주한, 팀원: 송성민<br><br>조원들과 토론 결과 요즘 음식점들은 개인 사이트를 원하는 경우가 많기에 수요가 또한 많을 것으로 보고 유명 프랜차이즈 레스토랑인 VIPS 사이트의 클론 코딩을 진행<br><br><a href='./file/project1.pdf'>상세문서</a>" },
   secondProject: { details: "어쩌구2 저쩌구~2" },
 };
 
